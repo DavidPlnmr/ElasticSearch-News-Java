b2555bc153412bdb36ab72c51c98425f214b486a
David Paulino <david.plnmr@gmail.com>
2021-12-09 16:28:47 +0100

    Added dotenv in the project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
 
-import org.elasticsearch.client.*;
+import javax.print.event.PrintEvent;
 
 import dao.Bdd;
 import dao.BddElasticSearch;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+import io.github.cdimascio.dotenv.internal.DotenvReader;
 
 public class Main {
     public static void main(String[] args) {
-        Bdd bdd = new BddElasticSearch("xxx", "yyy", "localhost", 9200, "http");
+        Dotenv dotenv = Dotenv.load();
+
+        Bdd bdd = new BddElasticSearch(dotenv.get("DB_USERNAME"), dotenv.get("DB_PASSWORD"), dotenv.get("DB_HOSTNAME"),
+                9200, "http");
 
     }
 }
